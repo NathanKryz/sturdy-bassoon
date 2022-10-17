ce82aec69be72f0320e744d7fade9cceb30e55cf
NathanKryz <112029666+NathanKryz@users.noreply.github.com>
2022-10-17 16:37:58 -0400

    minimum viable product release v1.0 featuring delete function

--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,6 @@
 const notes = require('express').Router();
-const path = require('path');
 const { v4: uuidv4 } = require('uuid');
-const { readAndAppend, readFromFile} = require('../helpers/fsfilehelp');
+const { readAndAppend, readFromFile, writeToFile} = require('../helpers/fsfilehelp');
 
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
@@ -9,9 +8,34 @@ notes.get('/', (req, res) => {
 
 
 notes.post('/', (req, res) => {
-    console.log(req.body);
 
+    const {title, text} = req.body;
 
-})
+    if(req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note appended to the list!`);
+    }
+    else {
+        res.error('Error in adding the note!');
+    }
+
+});
+
+notes.delete('/:id', async (req, res) => {
+    const {id} = req.params;
+    let currentArray = [];
+    await readFromFile('./db/db.json',).then((data) => currentArray = JSON.parse(data));
+
+    const deletedArray = currentArray.filter((note) => note.id !== id);
+    
+    writeToFile('./db/db.json', deletedArray);
+    res.json(`Note has been deleted`);
+});
 
 module.exports = notes;
